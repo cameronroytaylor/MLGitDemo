14024257e0807a392a9257f828dcf60895279332
Cameron Roy Taylor <cameronroytaylor@gmail.com>
2017-12-19 13:14:44 -0500

    Cameron is trying to break the system

--- helloWorld.py
+++ helloWorld.py
@@ -6,7 +6,12 @@ print "hello world"
 print "it works"
 
 print "Just making some small changes"
-print "shouldnt ruin anything right??"
-print "git is the best"
+print "That shouldn't ruin anything right??"
+print "git is the best."
 
-print "I hope no one breaks this"
+print "I hope no one break this, but Cameron is going to try."
+
+
+
+
+
